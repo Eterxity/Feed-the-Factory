ab0d0f67eef0011a66a2027e0eec49d06d151d00
Mykull <mikeyang5406@gmail.com>
2023-11-15 19:08:02 -0500

    HOLY SHIT GOT DROPPER WORKING

--- Scripts/basic_dropper.cs
+++ Scripts/basic_dropper.cs
@@ -3,8 +3,23 @@ using System;
 
 public partial class basic_dropper : Node3D
 {
-	
 	[Export]
 	private PackedScene _ore;
 
+	[Export]
+	private Timer _timer;
+
+	[Export]
+	private MeshInstance3D _spawner;
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta){
+
+	}
+
+	public void _on_timer_timeout(){
+		Node3D oreInstance = _ore.Instantiate<Node3D>();
+		oreInstance.Position = _spawner.Position;
+		CallDeferred("add_child", oreInstance);
+	}
 }
